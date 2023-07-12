1287afefcf753e4574027ea519c7ef95bdbcdf0b
Kuba <67916528+Kubaplay07@users.noreply.github.com>
2023-07-12 13:24:15 +0200

    react-native-config

---
+++ windows/Chatter/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Chatter::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Chatter::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
